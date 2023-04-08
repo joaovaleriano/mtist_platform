799bec5f876e1cda60da1a1ee1f48b2c333ee78c
joaovaleriano <joaopedrovm98@gmail.com>
2023-04-08 17:18:11 -0300

    .

--- mtist1.0/create_mtist_joao.py
+++ mtist1.0/create_mtist_joao.py
@@ -2,6 +2,7 @@ import os
 
 from mtist import master_dataset_generation as mdg
 from mtist import assemble_mtist as am
+from mtist import mtist_utils as mu
 
 import numpy as np
 
@@ -9,13 +10,15 @@ import numpy as np
 # GENERATE MTIST              #
 ###############################
 
-mu.MASTER_DATASET_DIR = "master_datasets_joao"
-mu.GT_DIR = "ground_truths_joao"
-mu.MTIST_DATASET_DIR = "mtist_datasets_joao"
+mu.GLOBALS.MASTER_DATASET_DIR = "master_datasets_joao"
+mu.GLOBALS.GT_DIR = "ground_truths_joao"
+mu.GLOBALS.MTIST_DATASET_DIR = "mtist_datasets_joao"
 
-am.N_TIMESERIES_PARAMS = np.arange(1, 21)
-am.SAMPLING_FREQ_PARAMS = [10, 20, 30]
-am.SAMPLING_SCHEME_PARAMS = ["even"]
+am.ASSEMBLE_MTIST_DEFAULTS.N_TIMESERIES_PARAMS = np.arange(1, 21, 4)
+am.ASSEMBLE_MTIST_DEFAULTS.SAMPLING_FREQ_PARAMS = [10, 20, 30]
+am.ASSEMBLE_MTIST_DEFAULTS.SAMPLING_SCHEME_PARAMS = ["even"]
+
+mdg.MASTER_DATASET_DEFAULTS.NOISE_SCALES = [0.1]
 
 mdg.generate_mtist_master_datasets()
 
